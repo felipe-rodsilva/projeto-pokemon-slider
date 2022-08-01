3cf149a8383a19537c5c8436d5b0a4baba36a11c
Felipe Rodrigues <felipeweb2309@gmail.com>
2022-08-01 18:19:30 -0300

    JS Update

--- src/js/index.js
+++ src/js/index.js
@@ -33,7 +33,9 @@ function mostrarCartao(indiceCartao) {
 }
 
 btnAvancar.addEventListener("click", function () {
-  if (cartaoAtual === cartoes.length - 1) return;
+  if (cartaoAtual === cartoes.length - 1) {
+    cartaoAtual = - 1
+  }
 
   esconderCartaoSelecionado();
 
@@ -42,7 +44,9 @@ btnAvancar.addEventListener("click", function () {
 });
 
 btnVoltar.addEventListener("click", function () {
-  if (cartaoAtual === 0) return;
+  if (cartaoAtual <=0) {
+    cartaoAtual = cartoes.length
+  }
 
   esconderCartaoSelecionado();
 
